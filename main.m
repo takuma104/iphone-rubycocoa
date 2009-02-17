9c722e9fcf6e056d92457ac301572276824a180b
takuma <takuma104@gmail.com>
2009-02-18 01:31:51 +0900

    ruby static linked

--- main.m
+++ main.m
@@ -1,5 +1,12 @@
 #import <UIKit/UIKit.h>
+#import "ruby.h"
 
 int main(int argc, char *argv[]) {
+    {
+        RUBY_INIT_STACK
+        ruby_init();
+        ruby_options(argc, argv);
+        ruby_run();
+    }
     return 0;
 }
